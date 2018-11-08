74d5b14dc85b7d96c830d7072965678d10f1d550
Hannie Kim <hanniek00@gmail.com>
2018-11-08 09:50:39 -0600

    Initial Commit

---
+++ goalpost-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Hannie Kim on 11/8/18.
+//  Copyright © 2018 Hannie Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
