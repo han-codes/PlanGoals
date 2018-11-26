1f95644c86cbe98d41773598ce59c622cfb671a6
Hannie Kim <hanniekim@Hannies-MacBook-Pro.local>
2018-11-26 07:35:47 -0600

    Create Class for TableViewCell

---
+++ goalpost-app/View/GoalCell.swift
@@ -0,0 +1,22 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Hannie Kim on 11/26/18.
+//  Copyright © 2018 Hannie Kim. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+    
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, progressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: progressAmount)
+    }
+}
